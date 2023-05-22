60699fc7f64114e7be3166a73ac5d2926c0b52c7
Vitao <victorf_16@hotmail.com>
2023-05-22 12:23:09 -0300

    Site Funcional

--- site/fideliza.py
+++ site/fideliza.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
+# Get the Docker host name
+docker_host = socket.gethostname()
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', host=docker_host)
+
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('signup.html', host=docker_host)
+
 
 if __name__ == '__main__':
     app.run()
